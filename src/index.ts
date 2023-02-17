dc5aeadbf43f6ba3407595043026ee3a6b1cd75a
great-park <cksgh1735@gmail.com>
2023-02-18 03:03:45 +0900

    fix: remove typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import '@/config/env';
 import express from '@/config/express';
 import logger from '@/config/winston';
-import AppDataSource from '@/config/data-source';
+// import AppDataSource from '@/config/data-source';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
 const port = process.env.PORT || 8080;
+express().listen(port, () => {
+  logger.info(`environment : ${process.env.NODE_ENV} - API Server Start At Port ${port}`);
+});
 
-AppDataSource.initialize()
-  .then(() => {
-    logger.info('DB Connected');
-    express().listen(port);
-    logger.info(`environment : ${process.env.NODE_ENV} - API Server Start At Port ${port}`);
-  })
-  .catch((error) => {
-    logger.error('DB Connection Fail');
-    logger.error(error.toString());
-  });
+// AppDataSource.initialize()
+//   .then(() => {
+//     logger.info('DB Connected');
+//     express().listen(port);
+//     logger.info(`environment : ${process.env.NODE_ENV} - API Server Start At Port ${port}`);
+//   })
+//   .catch((error) => {
+//     logger.error('DB Connection Fail');
+//     logger.error(error.toString());
+//   });
