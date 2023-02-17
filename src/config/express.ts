b8ec20f5073e15a5ef3772897509d36e19d99a0e
great-park <cksgh1735@gmail.com>
2023-02-18 00:03:48 +0900

    feat: config - 1.data source 2.express 3.logging 4.env

---
+++ src/config/express.ts
@@ -0,0 +1,24 @@
+import express, { json, urlencoded } from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import methodOverride from 'method-override';
+import router from '@/routes/index';
+import errorHandler from '@/middleware/errorHandler';
+
+const exp = () => {
+  const app = express();
+
+  app
+    .use(json())
+    .use(urlencoded({ extended: true }))
+    .use(compression())
+    .use(cors())
+    .use(methodOverride())
+    .use(express.static('public'));
+
+  app.use(router);
+  app.use(errorHandler);
+  return app;
+};
+
+export default exp;
