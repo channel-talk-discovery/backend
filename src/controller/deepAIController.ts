92f5a8d3157294de47f839b4b6c220629411c7c8
great-park <cksgh1735@gmail.com>
2023-02-18 07:35:43 +0900

    fix: hotfix

--- src/controller/deepAIController.ts
+++ src/controller/deepAIController.ts
@@ -1,12 +1,16 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import calculateImageSimilarity from '@/util/image_similarity_calculator';
 import { getPlaceInfo } from '@/service/placeService';
 import { infoDto } from '@/dto/placeInfoDto';
 
-export const getImageSimilarity = async (req: Request, res: Response): Promise<void> => {
-  const { placeId, uploadImageUrl } = req.body;
+export const getImageSimilarity = async (req: Request, res: Response, next: NextFunction,): Promise<void> => {
+  try {
+    const { placeId, uploadImageUrl } = req.body;
 
-  const response = await calculateImageSimilarity(placeId, uploadImageUrl);
+    const response = await calculateImageSimilarity(placeId, uploadImageUrl);
 
-  res.send(response);
+    res.send(response);
+  } catch (err) {
+    next(err)
+  }
 };
